632bd64c9efdc3e1fa46d825a6af9df81b231b53
keithro <rodriguez.keith@gmail.com>
2022-05-08 22:38:51 -0500

    Added google ReCAPTCHA to contact form

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,11 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const mail = require('@sendgrid/mail');
+const mail = require("@sendgrid/mail");
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
   const body = JSON.parse(req.body);
-  console.log(body);
+  // console.log(body);
+
+  const isHuman = await validateHuman(body.token);
+  if(!isHuman) {
+    res.status(400).json({ errors: ["Cannot validate user"]});
+    return;
+  }
 
   const message = `
     Name: ${body.name}\r\n
@@ -14,14 +20,25 @@ export default async function handler(req, res) {
   `;
 
   const data = {
-    to: 'rodriguez.keith@gmail.com',
-    from: 'hello@keithrodriguez.dev',
-    subject: 'New contact form message!',
+    to: "rodriguez.keith@gmail.com",
+    from: "hello@keithrodriguez.dev",
+    subject: "New contact form message!",
     text: message,
-    html: message.replace(/\r\n/g, '<br>')
-  }
+    html: message.replace(/\r\n/g, "<br>"),
+  };
 
   await mail.send(data);
-  
-  res.status(200).json({ status: 'Ok' });
+
+  res.status(200).json({ status: "Ok" });
+}
+
+async function validateHuman(token) {
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    { method: "POST" }
+  );
+  const data = await response.json();
+  // console.log("recaptcha response data: ", data);
+  return data.success;
 }
