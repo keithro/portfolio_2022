95b07a159688f3c56a40ffc2d6d88bee331f0f44
keithro <rodriguez.keith@gmail.com>
2022-05-15 23:01:03 -0500

    Hero mobile styling

--- components/Hero.js
+++ components/Hero.js
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./../styles/Hero.module.scss";
 
 const Hero = () => {
   return (
     <section id="home" className={styles.hero}>
-      <h2>A Full Stack Engineer</h2>
-      <h1>That Brings It All Together</h1>
-      <h2>A Full Stack Engineer</h2>
+      <div className={styles.content}>
+        <h2>A Full Stack Engineer</h2>
+        <h1>That Brings It <br /> All Together</h1>
+        <h2>I&#8217;m Keith Rodriguez</h2>
+        <div className={styles.buttons}>
+          <Link href="#contact">
+            <a className={styles.contact}>Contact Me</a>
+          </Link>
+          <Link href="#projects">
+            <a className={styles.projects}>My Portfolio</a>
+          </Link>
+        </div>
+      </div>
 
-      <Link href="#projects">
-        <a className="nav-link">Projects</a>
-      </Link>
-      <Link href="#contact">
-        <a className="nav-link">Contacts</a>
-      </Link>
+      {/* <img src="/bg-layer-1.svg" alt="Background Layer 3"/>
+      <img src="/bg-layer-2.svg" alt="Background Layer 3"/>
+      <img src="/bg-layer-3.svg" alt="Background Layer 3"/> */}
+      {/* <Image src="/bg-layer-1.svg" alt="Background Layer 3" width={1000} height={1000} />
+      <Image src="/bg-layer-2.svg" alt="Background Layer 3" width={1000} height={1000} />
+      <Image src="/bg-layer-3.svg" alt="Background Layer 3" width={1000} height={1000} /> */}
     </section>
   );
 };
